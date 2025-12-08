5b48b31b26242f49eb2225cc0722470732335c68
samulinukala <samulinukala740@gmail.com>
2025-12-08 15:18:05 +0200

    made progress

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
- 
-
+ // 6,203,132
+const style={flexDirection: "column", display: "flex",paddingTop: "10%",paddingBottom: "20%",backgroundColor: "rgb(6,203,132)"}
   return (
     
-      <div>
+      <div style={style}>
       
-      <h1>Samuli Nukala</h1>
-      <p> Recently graduated programmer that is learning react to get into frontend development. Has experience in programming games for museums. Likes to jump head first into problems</p>
+      <h1 style={{textAlign:"center",top: "25%"}}>Samuli Nukala</h1>
+    
+      <p style={{fontSize: "1.5em",marginLeft: "15%",maxWidth: "70%"}}> Recently graduated programmer that is learning react to get into frontend development. Has experience in programming games for museums. Likes to jump head first into problems</p>
+      
+      <div style={{marginTop: "3%",marginLeft: "10%",display: "flex"}}>
       <Skills text="problemSolving " emojis={<span>&#x1f635;</span>} emoji="🌚"></Skills>
+      <Skills text="problemSolving " emojis={<span>&#x1f635;</span>} emoji="🌚"></Skills>
+      <Skills text="problemSolving " emojis={<span>&#x1f635;</span>} emoji="🌚"></Skills>
+      <Skills text="problemSolving " emojis={<span>&#x1f635;</span>} emoji="🌚"></Skills>
+      </div>
        </div>
     
   )
 }
-
+// 203, 132, 54
 function Skills(props){
-
+const style={ fontSize: "1.2em",margin: "1%",padding: "6%",backgroundColor: "rgb(200,200,110)"}
   return(
-    <div className="Skills">
-    <p1>{props.text}</p1>
-    <p1>{props.emoji}</p1>
-    <p1>{props.emojis}</p1>
+    
+    <div style={{outerWidth: "120%"}}>
+    <p1 style={style}>{props.text+props.emoji}</p1>
+   
     </div>
   
-  )
+  );
 }
 
 export default App
